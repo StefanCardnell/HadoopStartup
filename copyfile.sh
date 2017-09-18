b58fcd0341293af5afd29e09167617795c0109d0
StefanCardnell <raging_rush@hotmail.co.uk>
2017-09-18 21:25:58 +0000

    Filename argument

--- copyfile.sh
+++ copyfile.sh
@@ -5,16 +5,27 @@
 
 # See https://www.cloudera.com/documentation/enterprise/5-7-x/topics/admin_hdfs_balancer.html
 
+if [ -z "$1" ]
+then
+    echo "No file to copy supplied"
+    exit 1
+fi
+
+filename=`basename $1`
+
+echo -e "##################\nDeleting existing file"
+hadoop dfs -rmr /$filename
+
 echo -e "##################\nCopying file across"
-hadoop dfs -copyFromLocal wordcountfiles/mediumfile.txt /mediumfile.txt
+hadoop dfs -copyFromLocal $1 /$filename || exit 1
 
 echo -e "##################\nObtaining balancer threshold"
 
-blocksize=`sed -n '/block.size/{n;p}' /opt/hadoop/conf/hdfs-site.xml | sed -n 's/.*>\([0-9]\+\)<.*/\1/p'` # Obtain the blocksize
-filesize=`df -B1 / | tail -n 1 | awk '{print $2;}'` # Obtain the partitions total size
+blocksize=`hadoop dfs -stat %o /$filename` # Obtain the blocksize
+filesize=`df -B1 / | tail -n 1 | awk '{print $2;}'` # Obtain the partition's total size
 
-percentage=`echo "$blocksize*50/$filesize" | bc -l`
-percentage=`printf "%.3g" $percentage`  # Two significant figures
+percentage=`echo "scale=20;$blocksize*50/$filesize" | bc`
+percentage=`printf "%.6f" $percentage`  # Two significant figures
 echo "Balancer threshold of $percentage"
 
 echo -e "###################\nDistributing blocks"
@@ -23,5 +34,4 @@ hadoop balancer -threshold $percentage
 
 echo -e "###################\nBalancer finished. Outputting new distribution..."
 
-hadoop fsck /mediumfile.txt -files -blocks -racks
-
+hadoop fsck /$filename -files -blocks -racks
